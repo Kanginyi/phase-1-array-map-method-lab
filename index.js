db86a368716f18a74ff1eefd8144c724657a3c35
Eric Yi <erickanginyi@gmail.com>
2021-09-05 00:51:24 -0400

    commit tree 31e5fde8398c06dcc717e86b44666398555133b1

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(z => z.split(" ")
+  //Each string within the initial tutorials array has been changed with .split(" ") to an array with each word "split"
+  //["what", "does", "the", "this", "keyword", "mean?"
+
+  .map(x => x.replace(x.charAt(0), x.charAt(0).toUpperCase()))
+  //Have a nested map on the returned split array; each element is also an array, so we can use another .map()
+  //We want to use the string method of replace for each first character at (charAt) the index of 0 for the string
+  //Change each of those to upperCase()
+  
+  .join(" "));
+  //Each element inside of the new, mapped, returned array is still an array, so we want to bop that shit back to being a string
+  //Use .join(" ") and it should return the answer
+  //This shit took me way too fucking long, fuck you guys for making me struggle and making me sad :^( jk ily
 }
